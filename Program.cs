31d75e7dbcd6272b3a3f8418b69e8b193eb5938d
isacgustavsson <117119420+isacgustavsson@users.noreply.github.com>
2026-01-29 10:59:26 +0100

    add DbContext

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
